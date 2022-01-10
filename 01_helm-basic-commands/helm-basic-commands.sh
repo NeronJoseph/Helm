022b6ac8a902b461ee6de5c34f8b133f71077467
Neron Joseph <njoseph@menervasoftware.com>
2022-01-10 22:35:45 +0530

    Updated helm-basic-commands file with some more commands

--- 01_helm-basic-commands/helm-basic-commands.sh
+++ 01_helm-basic-commands/helm-basic-commands.sh
@@ -5,3 +5,30 @@ helm version
 
 #To get more details about Helm
 helm --help
+
+#To get help for a sub command
+helm repo --help
+
+#To get help for a sub sub command
+helm repo update --help
+
+#Search for wordpress chart from the artifact hub
+helm search hub wordpress
+
+#Add a repository to your helm
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+#Install a wordpress from the repo with name my-release
+helm install my-release bitnami/wordpress
+
+#List all exisiting releases
+helm list 
+
+#Delete a release with name my-release
+helm uninstall my-release
+
+#List repositories in your helm
+helm repo list
+
+#Update local repository
+helm repo update
